56fc1654f38810f7986b9c076c9bee2bc7504070
Slozzyondul <solomonondula.com>
2024-01-25 17:59:27 +0300

    Initial commit4

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,10 +1,27 @@
-from flask import Flask, jsonify, request, url_for, redirect, session, render_template
+from flask import Flask, jsonify, request, url_for, redirect, session, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
 app.config['SECRET_KEY'] = 'secret!'
+
+def connect_db():
+    sql = sqlite3.connect('/home/solo/Desktop/Flask_app/data.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db    
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+        
 
 @app.route('/')
 def index():
@@ -15,7 +32,13 @@ def index():
 @app.route('/home/<string:name>', methods=['POST', 'GET'])
 def home(name): 
     session['name'] = name
-    return render_template('home.html', name=name, display=False, mylist=['one','two','three','four'], listofdictionaries=[{'name' : 'Solomon_Ondula'}, {'name' : 'Solomon_omusinde'}])
+    #displaying database in the home page 
+    db = get_db()
+    cur = db.execute('select id, name, location from users')
+    results = cur.fetchall()
+
+    return render_template('home.html', name=name, display=False, \
+                           mylist=['one','two','three','four'], listofdictionaries=[{'name' : 'Solomon_Ondula'}, {'name' : 'Solomon_omusinde'}], results = results)
 
 @app.route('/json') 
 def json():
@@ -40,6 +63,10 @@ def theform():
         name = request.form['name']
         location = request.form['location']
 
+        db = get_db()
+        db.execute('insert into users (name, location) values (?, ?)', [name, location])
+        db.commit()
+
         return redirect(url_for('home', name=name, location=location))
 
 
@@ -60,6 +87,13 @@ def processjson():
     randomlist = data['randomlist']
 
     return jsonify({'result' : 'Success!', 'name':name, 'location':location, 'randomkeyinlist':randomlist[1]})
+
+@app.route('/viewresults')
+def viewresults():
+    db = get_db()
+    cur = db.execute('select id, name, location from users')
+    results = cur.fetchall()
+    return '<h1> The ID is {}. The name is {}. The location is {}.</h1>'.format(results[1]['id'], results[1]['name'], results[1]['location'])
 
 if __name__ == '__main__':
     app.run()
