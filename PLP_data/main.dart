e8e62f849c29dfa9df97590ca198a2d15d03fcb5
Slozzyondul <solomonondula.com>
2024-02-23 17:55:22 +0300

    Initial commit24

--- PLP_data/main.dart
+++ PLP_data/main.dart
@@ -1,9 +1,9 @@
-//void main() {
-//print("Hello World Welcome to Solomons world");
-//}
+/*void main() {
+print("Hello World Welcome to Solomons world");
+}*/
 
 //Variables In Dart
-void main() {
+/*void main() {
   // declaring variables
   String name = "Solomon"; //oughts to be in quotes
   String address = "Nairobi"; //string must be in quotes
@@ -17,4 +17,115 @@ void main() {
   print("Age is $age");
   print("Height is $height");
   print("Married Status is $isMarried");
+}*/
+
+/*
+// create a functions that shows sum of two numbers
+void main() {
+  print(sumOfTwoNumbers(5, 7)); // Example usage: prints 12
+}
+
+int sumOfTwoNumbers(int num1, int num2) {
+  return num1 + num2;
+}*/
+
+//Data Types in Dart(Numbers)
+
+/*
+void main() {
+  //declaring variables
+  int num1 = 100; // without decimal point.
+  double num2 = 130.2; //with decimal point.
+  num num3 = 50;
+  num num4 = 50.4;
+
+
+
+// for sum
+  num sum = num1 + num2 + num3 + num4;
+
+  // printi all the data out
+  print("Num 1 is $num1");
+  print("Num 1 is $num2");
+  print("Num 1 is $num3");
+  print("Num 1 is $num4");
+  print("Sum is $sum");
+}
+*/
+
+/*
+// Data Types in dart(String)
+void main() {
+  //declaring Values
+  String schoolName = "Solomon's School of home learning";
+  String address = "Nairoobi, Kenya";
+  // printing the results
+  print("My School name is $schoolName and the address is $address");
+}
+*/
+
+/*
+// Data Types in dart(Boolean)
+void main() {
+  bool isMarried = true;
+  print("Married Status: $isMarried");
+}
+*/
+
+/*
+// Data Types in dart(Lists)
+void main() {
+  List<String> names = ["John", "Solo", "Peter"];
+  print("value of names is $names");
+  print("Value of names[0] is ${names[0]}"); //position/index 0
+  print("Value of names[1] is ${names[1]}"); //index 1
+  print("Value of names[3] is ${names[2]}"); //index 2
+
+  print(names);
+}
+*/
+
+/*
+// Data Types in dart(Maps)
+void main() {
+  //creating a map with string keys and int values
+  Map<String, int> ages = {'Alice': 30, 'Solo': 27, 'Charlie': 35};
+  print("Ages of students: $ages");
+}
+*/
+
+/*
+// Data Types in dart(Runes)
+void main() {
+  //define a string with runes
+  String runesString = "Runes in Dart: \u{1F600} \u{1F64B} \u{1F680} \u{0041}";
+
+  // print the string
+  print(runesString);
+}
+*/
+
+//arthimetic operations using numbers
+void main() {
+  // declaring two numbers
+  int num1 = 10;
+  int num2 = 3;
+
+  //performing arithmetic calculation
+  int sum = num1 + num2; //addition
+  int diff = num2 - num1; //unary minus
+  int subtract = num1 - num2; //subtraction
+  int mul = num1 * num2; // multiplication
+  double div = num1 / num2; // division
+  int div2 = num1 ~/ num2; // integer division
+  int mod = num1 % num2; // show remainder
+
+  // printing info
+  print("The addition is $sum.");
+  print("The unary is $diff.");
+  print("The subtraction is $subtract");
+  print("The multiplication is $mul.");
+  print("The division is $div.");
+  print("The integer division is $div2.");
+  print("The modulus is $mod.");
 }
